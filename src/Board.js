b7ace1deb660d7300d5bf89cf0865bddc56ff74a
lee-philip-n <lee.philip.n@gmail.com>
2018-01-24 18:58:58 -0800

    finished board.js and first part of solvers.js

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,34 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+    //find our rows
+    //create a count variable
+    //iterate through the row
+      //if we find a trigger
+        //increase count
+    //if count > 1 return true
+      let count = 0;
+      let currentRow = this.get(rowIndex);
+      for (let i = 0; i < this.get('n'); i++) {
+        if (currentRow[i] === 1) {
+          count++;
+        }
+      }
+      return count > 1;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      // create a for loop iterating through all rows
+        // if hasRowConflictAt for each row iterated
+          //return true
+        // else return false 
+      for (let i = 0; i < this.get('n'); i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -94,12 +116,34 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      //create count
+      //iterate through each row
+        //for each row check if colIndex is 1 
+          //if true count++
+      //return count > 1
+
+      let count = 0;
+      for (let i = 0; i < this.get('n'); i++) {
+        let row = this.get(i);
+        if (row[colIndex] === 1) {
+          count++;
+        }
+      }
+      return count > 1;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      //iterate through each row
+        //if hasColConflictAt(i) true
+         //return true
+        //else false
+      for (let i = 0; i < this.get('n'); i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -109,27 +153,83 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      //create count 
+      //row = 0
+      //for loop iterate index
+      let count = 0;
+      let row = 0;
+      let column = majorDiagonalColumnIndexAtFirstRow;
+      for ( let i = 0; i < this.get('n'); i++) {
+        if (this.get(row)[column] === 1) {
+          count++;
+        }
+        row++;
+        column++;
+      }
+      return count > 1; // fixme
+
+      
     },
 
     // test if any major diagonals on this board contain conflicts
+    
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      //iterate through index
+        //call hasMajorDiagonalConflictAt(index)
+      //iterate rows
+      for (var i = -1 * (this.get('n') - 1); i < this.get('n'); i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
-
+ 
 
     // Minor Diagonals - go from top-right to bottom-left
     // --------------------------------------------------------------
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      //create count to keep track of conflicts
+      //variable to keep track of input
+      //varaible to keep track of row
+      //iterate through each index
+        //compare input with a conflict
+          //increase count if conflict
+      //decrease input variable to check next index
+      //increase row to go to next row
+      //return if count is > 1
+      let count = 0;
+      let row = 0;
+      let column = minorDiagonalColumnIndexAtFirstRow;
+      for (var i = 0; i < this.get('n'); i++) {
+        if (this.get(row)[column] === 1) {
+          count++;
+        }
+      row++;
+      column--;
+      }
+      return count > 1;
+          
+
+
+    
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      //iterate through each index in the first row
+        //callback hasMinorDiagonalConflictAt
+        //return true callback is true
+        //else false
+      for (var i = this.get('n') + (this.get('n') - 1); i > 0; i--) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
--- src/solvers.js
+++ src/solvers.js
@@ -16,7 +16,47 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = []; 
+  //input: number of pieces
+  //output: a matrix representing a single chessboard
+  
+  //create a new board by nxn
+  //loop n number of times
+    //place a toggle a piece randomly
+      //check conflicts
+      //if conflict toggle piece again and revert loop one time
+      //randomly toggle another piece
+        //rinse and repeat (recursion?)
+  
+  // let board = new Board({ 'n': n});
+  // let randomRow = Math.floor(Math.random() * n);
+  // let randomColumn = Math.floor(Math.random() * n);    
+  // for (var i = 0; i < n; i++) {
+  //   board.togglePiece(randomRow, randomColumn);
+  //   let storedRow = randomRow;
+  //   let storedColumn = randomColumn;
+  //   if (board.hasAnyRookConflicts) {
+  //     board.togglePiece(storedRow, storedColumn);
+  //     i--;
+  //   } 
+  // }
+
+  // 'i' for loop to repeat n times
+  //create an array
+    // 'j' for loop to iterate over index
+      // if i = j insert 1 
+      // else insert 0
+  for (let i = 0; i < n; i++) {
+    let array = [];
+    for (let j = 0; j < n; j++) {
+      if (j === i) {
+        array.push(1);
+      } else {
+        array.push(0);
+      }
+    }
+    solution.push(array);
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
